9bffc5af9d2abc65fd196e5c7cad87b69d2b260a
Supakit Jaratworapat <supakit.jaratworapat@gmail.com>
2025-09-16 16:41:24 +0700

    feat: Add Step component and DoneRight icon
    - Created a new DoneRight icon component for visual representation.
    - Implemented a Step component that utilizes the DoneRight icon to indicate step status.
    - Updated the index page to import and use the Step component.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import { Button } from "@/components/ui/button";
 import { useEffect } from "react";
 import { toast } from "react-toastify";
+import Step from "@/components/Steps/Step";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",

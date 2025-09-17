43201c0eb06b6aed7dade5aac07c17d35230bd2b
bestnp <narawit.p40@gmail.com>
2025-09-17 10:33:02 +0700

    fix: correct stroke-linecap attribute casing in AddRoundLight component

--- src/components/Icons/AddRoundLight.tsx
+++ src/components/Icons/AddRoundLight.tsx
@@ -19,8 +19,8 @@ const AddRoundLight: React.FC<AddRoundLightProps> = ({
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
     >
-      <path d="M12 6L12 18" stroke={color} stroke-linecap="round" />
-      <path d="M18 12L6 12" stroke={color} stroke-linecap="round" />
+      <path d="M12 6L12 18" stroke={color} strokeLinecap="round" />
+      <path d="M18 12L6 12" stroke={color} strokeLinecap="round" />
     </svg>
   );
 };

cb680690e49c0e3f39bd3dabf80f0e1e9e695f35
PHET45 <phnjk2000@gmail.com>
2025-09-17 10:16:03 +0700

    feat: add modal card

--- src/pages/all-widget.tsx
+++ src/pages/all-widget.tsx
@@ -51,12 +51,41 @@ const AllWidget = () => {
           </div>
         </div>
       </div>
+      {/* Modal */}
+      <div className='flex flex-col items-center '>
+        <div className="bg-[#21263F] rounded-lg shadow-xl max-w-md w-[343px] h-[194px]  relative ">
+          {/* Close Button */}
+          <button className="absolute top-5 right-10 text-white hover:text-gray-300 transition-colors cursor-pointer">
+            X
+          </button>
+          {/* Modal Content */}
+          <div className="p-5 items-center text-center">
+            <h2 className="flex flex-col items-center text-white text-xl font-semibold mb-4">
+              Modal Title
+            </h2>
+
+            <p className="text-gray-300 text-sm leading-relaxed mb-5 w-full">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+            </p>
+
+            <div className="flex flex-row items-center justify-center gap-7">
+              {/* White Outline Button */}
+              <Button className="btn-base white-outline-normal">Button</Button>
+
+              {/* Blue Button */}
+              <Button className="btn-base blue-normal">Button</Button>
+            </div>
+          </div>
+        </div>
+      </div>
       <div className="mt-10 flex flex-col items-center gap-5">
         <h1 className="text-xl font-bold">Step</h1>
         <div className="flex gap-10">
           <div className="stp-default">1</div>
           <div className="stp-current">2</div>
-          <div className="stp-done"><DoneRight /></div>
+          <div className="stp-done">
+            <DoneRight />
+          </div>
         </div>
       </div>
 
@@ -78,7 +107,7 @@ const AllWidget = () => {
     </div>
 
     </div>
-  );
-};
+  )
+}
 
-export default AllWidget;
+export default AllWidget

ae3e07929b759cdc3677b84c3f6ab064565a3d2d
bestnp <narawit.p40@gmail.com>
2025-09-17 20:27:37 +0700

    feat: add DesignTokens and IconsGrid components, update all-widget page to include them

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -45,33 +45,18 @@ const config: Config = {
       },
       fontSize: {
         // Headline styles
-        "headline-1": ["3.5rem", { lineHeight: "4rem", fontWeight: "700" }], // 56px/64px
-        "headline-2": ["2.25rem", { lineHeight: "2.75rem", fontWeight: "700" }], // 36px/44px
-        "headline-3": ["1.5rem", { lineHeight: "1.875rem", fontWeight: "700" }], // 24px/30px
-        "headline-4": [
-          "1.25rem",
-          { lineHeight: "1.625rem", fontWeight: "700" },
-        ], // 20px/26px
+        "f-56": ["3.5rem", { lineHeight: "4rem", fontWeight: "700" }], // 56px/64px
+        "f-36": ["2.25rem", { lineHeight: "2.75rem", fontWeight: "700" }], // 36px/44px
+        "f-24": ["1.5rem", { lineHeight: "1.875rem", fontWeight: "700" }], // 24px/30px
+        "f-20": ["1.25rem", { lineHeight: "1.625rem", fontWeight: "700" }], // 20px/26px
 
         // Body styles
-        "body-1-medium": ["1rem", { lineHeight: "1.5rem", fontWeight: "500" }], // 16px/24px Medium
-        "body-1-regular": ["1rem", { lineHeight: "1.5rem", fontWeight: "400" }], // 16px/24px Regular
-        "body-2-medium": [
-          "0.875rem",
-          { lineHeight: "1.25rem", fontWeight: "500" },
-        ], // 14px/20px Medium
-        "body-2-regular": [
-          "0.875rem",
-          { lineHeight: "1.25rem", fontWeight: "400" },
-        ], // 14px/20px Regular
-        "body-3-medium": [
-          "0.75rem",
-          { lineHeight: "1.125rem", fontWeight: "500" },
-        ], // 12px/18px Medium
-        "body-3-regular": [
-          "0.75rem",
-          { lineHeight: "1.125rem", fontWeight: "400" },
-        ], // 12px/18px Regular
+        "fm-16": ["1rem", { lineHeight: "1.5rem", fontWeight: "500" }], // 16px/24px Medium
+        "fr-16": ["1rem", { lineHeight: "1.5rem", fontWeight: "400" }], // 16px/24px Regular
+        "fm-14": ["0.875rem", { lineHeight: "1.25rem", fontWeight: "500" }], // 14px/20px Medium
+        "fr-14": ["0.875rem", { lineHeight: "1.25rem", fontWeight: "400" }], // 14px/20px Regular
+        "fm-12": ["0.75rem",{ lineHeight: "1.125rem", fontWeight: "500" },], // 12px/18px Medium
+        "fr-12": ["0.75rem",{ lineHeight: "1.125rem", fontWeight: "400" },], // 12px/18px Regular
       },
       borderRadius: {
         lg: "var(--radius)",

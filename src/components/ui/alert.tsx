1413260261553f9dd032137675587df917220ea8
Jue Parinthon <jue.prt@hotmail.com>
2025-09-17 10:28:32 +0700

    refactor: replace button text with CloseRoundLight icon in ErrorAlert and SuccessAlert components

--- src/components/ui/alert.tsx
+++ src/components/ui/alert.tsx
@@ -1,9 +1,11 @@
+import CloseRoundLight from "../Icons/CloseRoundLight";
+
 export function ErrorAlert() {
   return (
     <div className="max-w-[480px] bg-[#E5364B]/60 p-4 rounded-sm">
       <div className="flex justify-between">
         <h1 className="font-bold">Attention needed</h1>
-        <button className="cursor-pointer">X</button>
+        <button className="cursor-pointer"><CloseRoundLight /></button>
       </div>
       <p className="text-sm mt-1">
         Lorem, ipsum dolor sit amet consectetur adipisicing elit. Reiciendis
@@ -18,7 +20,7 @@ export function SuccessAlert() {
     <div className="max-w-[480px] bg-[#00A372]/60 p-4 rounded-sm">
       <div className="flex justify-between">
         <h1 className="font-bold">Attention needed</h1>
-        <button className="cursor-pointer">X</button>
+        <button className="cursor-pointer"><CloseRoundLight /></button>
       </div>
       <p className="text-sm mt-1">
         Lorem, ipsum dolor sit amet consectetur adipisicing elit. Reiciendis

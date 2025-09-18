e675d69f0ad6b8ffd872275366ab2124426d68d4
Jue Parinthon <jue.prt@hotmail.com>
2025-09-18 18:50:44 +0700

    fix: fix conflict

--- src/components/ui/dateselection.tsx
+++ src/components/ui/dateselection.tsx
@@ -1,15 +1,18 @@
+import { useState } from "react";
+
 interface DateSelectionProps {
   day: string;
   date: string;
 }
 
 function DateSelection({day, date}: DateSelectionProps) {
+
     return (
         <>
-            <div className="w-fit h-fit hover:bg-gray-g63f flex flex-col py-2 px-16 items-center rounded-sm group">
+            <button className={`w-fit h-fit hover:bg-gray-g63f flex flex-col py-2 px-16 items-center rounded-sm group`}>
                 <h3 className="text-f-24 text-gray-g3b0 group-hover:text-white-wfff">{day}</h3>
                 <p className="text-fr-16 text-gray-gf7e group-hover:text-gray-gedd">{date}</p>
-            </div>
+            </button>
         </>
     )
 }

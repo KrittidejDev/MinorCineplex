e2ff89f85cf4c36ca0fff9fa1e3b7c578972c73a
Jue Parinthon <jue.prt@hotmail.com>
2025-09-18 15:46:21 +0700

    feat: add Date selection component and render in All-widget

---
+++ src/components/ui/dateselection.tsx
@@ -0,0 +1,17 @@
+interface DateSelectionProps {
+  day: string;
+  date: string;
+}
+
+function DateSelection({day, date}: DateSelectionProps) {
+    return (
+        <>
+            <div className="w-fit h-fit hover:bg-gray-g63f flex flex-col py-2 px-16 items-center rounded-sm group">
+                <h3 className="text-f-24 text-gray-g3b0 group-hover:text-white-wfff">{day}</h3>
+                <p className="text-fr-16 text-gray-gf7e group-hover:text-gray-gedd">{date}</p>
+            </div>
+        </>
+    )
+}
+
+export default DateSelection

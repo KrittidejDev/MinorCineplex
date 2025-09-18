bfdbc29beac81aec16e418721fd57e144424d5b4
PHET45 <phnjk2000@gmail.com>
2025-09-18 17:39:14 +0700

    feat: route to detail page and add nav in SpecialCoupons

--- src/components/Cards/CouponCard.tsx
+++ src/components/Cards/CouponCard.tsx
@@ -11,12 +11,12 @@ const CouponCard = ({ title, onClick }: CouponCardProps) => {
     <div 
       className="w-[161px] md:w-[285px] h-[337px] lg:w-[285px] lg:h-[477px] flex flex-col cursor-pointer rounded-[8px] bg-[#070C1B]"
     >
-      <div className="h-[285px] bg-[#FFFFFF] rounded-t-[8px]">
-        <img src="" alt={title} onClick={onClick}/>
+      <div className="h-[285px] bg-[#FFFFFF] rounded-t-[8px]" onClick={onClick}>
+        <img src="" alt={title} />
       </div>
       <div className="flex flex-col flex-1 items-center gap-y-9 p-4">
         <div className="flex flex-col mt-3 gap-3 items-start">
-          <h4 className="headline-4 text-[#FFFFFF] font-bold text-xl line-clamp-2" onClick={onClick}>{title}</h4>
+          <h4 className="headline-4 text-[#FFFFFF] font-bold text-xl line-clamp-2 hover:underline" onClick={onClick}>{title}</h4>
           <div className="flex gap-5">
             <p className="text-sm text-[#8B93B0]">Valid until</p>
             <p className="text-sm text-[#8B93B0]">31 Dec 2024</p>

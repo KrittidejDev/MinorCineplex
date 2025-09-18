ace8d0cbc6c31104776bab74904345e4cd6777ba
PHET45 <phnjk2000@gmail.com>
2025-09-18 18:37:02 +0700

    fix: delete onClick type

--- src/components/Cards/CouponCard.tsx
+++ src/components/Cards/CouponCard.tsx
@@ -3,12 +3,11 @@ import { Button } from '../ui/button'
 import { useRouter } from 'next/router'
 interface CouponCardProps {
   title: string
-  onClick?: () => void
 }
 
-const CouponCard = ({ title, onClick }: CouponCardProps) => {
+const CouponCard = ({ title}: CouponCardProps) => {
   const router = useRouter()
-    const handleClickcoupon = (id: string) => {
+    const handleClickcoupon = (id: number) => {
       router.push(`/coupons/${id}`)
     }
   
@@ -16,12 +15,12 @@ const CouponCard = ({ title, onClick }: CouponCardProps) => {
     <div 
       className="w-[161px] md:w-[285px] h-[337px] lg:w-[285px] lg:h-[477px] flex flex-col rounded-[8px] bg-[#070C1B]"
     >
-      <div className="h-[285px] bg-[#FFFFFF] rounded-t-[8px] cursor-pointer " onClick={() => handleClickcoupon('1')}>
+      <div className="h-[285px] bg-[#FFFFFF] rounded-t-[8px] cursor-pointer " onClick={() => handleClickcoupon(1)}>
         <img src="" alt={title} />
       </div>
       <div className="flex flex-col flex-1 items-center gap-y-9 p-4">
         <div className="flex flex-col mt-3 gap-3 items-start">
-          <h4 className="headline-4 text-[#FFFFFF] font-bold text-xl line-clamp-2 hover:underline cursor-pointer " onClick={() => handleClickcoupon('1')}>{title}</h4>
+          <h4 className="headline-4 text-[#FFFFFF] font-bold text-xl line-clamp-2 hover:underline cursor-pointer " onClick={() => handleClickcoupon(1)}>{title}</h4>
           <div className="flex gap-5">
             <p className="text-sm text-[#8B93B0]">Valid until</p>
             <p className="text-sm text-[#8B93B0]">31 Dec 2024</p>

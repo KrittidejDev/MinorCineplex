628c36df8ee0f6765465e88f7b000aa5e8ee93a3
PHET45 <phnjk2000@gmail.com>
2025-09-18 22:17:26 +0700

    feat: add layout detail coupons

--- src/pages/coupons/[id]/index.tsx
+++ src/pages/coupons/[id]/index.tsx
@@ -1,8 +1,15 @@
-import React from "react";
+import React from 'react'
 import NavAndFooter from '@/components/MainLayout/NavAndFooter'
 
 const CouponDetail = () => {
-  return <NavAndFooter>CouponDetail</NavAndFooter>;
-};
+  return (
+  <NavAndFooter>
+    <div className='flex flex-row w-full h-screen px-40 py-30 gap-5'>
+      <div className='w-1/3 h-full #8B93B0'><img src="/public/file.svg" alt="mock" /></div>
+      <div className='w-2/3 h-full bg-amber-100 border rounded-[6px]'></div>
+      </div>
+    </NavAndFooter>
+    )
+}
 
-export default CouponDetail;
+export default CouponDetail

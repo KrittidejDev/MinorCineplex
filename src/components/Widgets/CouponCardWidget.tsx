83184a818b12dc27a4c3ba146ba5a14e9a4f8be9
PHET45 <phnjk2000@gmail.com>
2025-09-18 17:55:42 +0700

    reflector: link to Coupon

--- src/components/Widgets/CouponCardWidget.tsx
+++ src/components/Widgets/CouponCardWidget.tsx
@@ -1,7 +1,7 @@
 import CouponCard from '../Cards/CouponCard'
-
 import React from 'react'
 import { Button } from '../ui/button'
+import Link from 'next/link'
 
 const Coupon = () => {
   return (
@@ -9,9 +9,11 @@ const Coupon = () => {
       <div className="flex flex-col gap-10  max-w-[1200px]">
         <div className="flex justify-between items-center font-bold text-2xl py-1  ">
           <h2 className="headline-2">Special coupons</h2>
-          <Button className="btn-base-transparent-underline-normal text-sm hover:underline">
-            View all
-          </Button>
+          <Link href="/coupons" passHref>
+            <Button className="btn-base-transparent-underline-normal text-sm hover:underline">
+              View all
+            </Button>
+          </Link>
         </div>
         <div className="lg:flex gap-5 md:grid-cols-3 md:gap-5 grid grid-cols-2 ">
           <CouponCard title={'Minor Cineplex x COKE JOYFUL '} />

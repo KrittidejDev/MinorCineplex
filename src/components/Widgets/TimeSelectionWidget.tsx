df270c1f571eaed6b559c574698f0f8e99941b72
Jue Parinthon <jue.prt@hotmail.com>
2025-09-18 22:56:02 +0700

    feat: add Movie detail widget and Time selection wedget into Movie detail page

--- src/components/Widgets/TimeSelectionWidget.tsx
+++ src/components/Widgets/TimeSelectionWidget.tsx
@@ -1,37 +1,20 @@
-import Tag from "./Tag";
-import LocationIconBlue from "../Icons/LocationIconBlue";
-import ShowtimeSelection from "./ShowtimeSelection";
-import ExpandDownLight from "../Icons/ExpandDownLight";
+import TimeSelectionCard from "../Cards/TimeSelectionCard";
 
 function TimeSelectionWidget() {
   return (
     <>
-      <div className="w-screen flex justify-center">
-        <div className="w-[1200px] bg-gray-gc1b flex flex-col rounded-md">
-          <div className="flex justify-between py-4 px-6 border-b border-gray-g63f">
-            <div className="flex items-center gap-5">
-              <LocationIconBlue />
-              <h3 className="text-f-24">Minor Cineplex Arkhem</h3>
-              <Tag name="Hearing assistance" />
-              <Tag name="Wheelchair access" />
-              <div>
-                <ExpandDownLight width={40} height={40} color={"#C8CEDD"} />
-              </div>
-            </div>
-          </div>
-          <div className="px-6 mt-10">
-            <h3 className="text-f-24 text-gray-gedd mb-4">Hall 1</h3>
-            <ShowtimeSelection />
-          </div>
-          <div className="px-6 mt-15">
-            <h3 className="text-f-24 text-gray-gedd mb-4">Hall 3</h3>
-            <ShowtimeSelection />
-          </div>
-          <div className="px-6 mt-15 mb-10">
-            <h3 className="text-f-24 text-gray-gedd mb-4">Hall 6</h3>
-            <ShowtimeSelection />
-          </div>
-        </div>
+      <div className="w-screen flex flex-col items-center gap-6 mb-20">
+        <TimeSelectionCard
+          cinemaName="Minor Cineplex Arkham"
+          halls={[1, 3, 6]}
+          tags={["Hearing assistance", "Wheelchair access"]}
+        />
+        <TimeSelectionCard cinemaName="Minor Riddler Factory" halls={[2, 5]} />
+        <TimeSelectionCard
+          cinemaName="Minor Cineplex Tricorner"
+          halls={[1]}
+          tags={["Hearing assistance", "Wheelchair access"]}
+        />
       </div>
     </>
   );

18588fa01fd5e5bff6321eb358277f8e53b8db69
Supakit Jaratworapat <supakit.jaratworapat@gmail.com>
2025-09-19 16:18:18 +0700

    feat: enhance signup API to validate username and phone number, and update response message

--- src/pages/api/auth/signup.ts
+++ src/pages/api/auth/signup.ts
@@ -6,14 +6,20 @@ const prisma = new PrismaClient();
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
 
-  const { email, password } = req.body;
+  const { email,useName,phoneNumber,password } = req.body;
   if (!email || !password) return res.status(400).json({ error: "Missing fields" });
 
-  const existing = await prisma.User.findUnique({ where: { email } });
-  if (existing) return res.status(400).json({ error: "Email already exists" });
+  const existingUsername = await prisma.user.findUnique({ where: { useName } });
+  if (existingUsername) return res.status(400).json({ error: "Username already exists" });
+  
+  const existingEmail = await prisma.user.findUnique({ where: { email } });
+  if (existingEmail) return res.status(400).json({ error: "Email already exists" });
+
+  const existingPhone = await prisma.user.findUnique({ where: { phoneNumber } });
+  if (existingPhone) return res.status(400).json({ error: "Phone Number already exists" });
 
   const hashed = await bcrypt.hash(password, 10);
-  const user = await prisma.User.create({ data: { email, password: hashed } });
+  await prisma.user.create({ data: { email, password: hashed } });
 
-  return res.status(201).json({ id: user.id, email: user.email });
+  return res.status(201).json({ message: "Register Sucessfully"});
 }

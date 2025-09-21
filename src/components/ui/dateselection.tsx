2b1cdd8fdaaa1cebff85b7cc7f266bcc3da3ef68
Jue Parinthon <jue.prt@hotmail.com>
2025-09-22 01:17:48 +0700

    fix: fix date color when active

--- src/components/ui/dateselection.tsx
+++ src/components/ui/dateselection.tsx
@@ -12,9 +12,9 @@ function DateSelection({day, date, isActive=false, onSelect, className=""}: Date
             <button onClick={onSelect} className={`w-fit h-fit hover:bg-gray-g63f flex flex-col py-2 px-[27.5px] lg:px-16 pl-4 items-center rounded-sm group ${className}
             ${isActive ? "bg-gray-g63f" : ""}`}>
                 <h3 className={`text-f-24 text-gray-g3b0 group-hover:text-white-wfff
-                    ${isActive ? "text-white-wfff" : ""}`}>{day}</h3>
+                    ${isActive ? "!text-white-wfff" : ""}`}>{day}</h3>
                 <p className={`text-fr-16 text-gray-gf7e group-hover:text-gray-gedd
-                    ${isActive ? "text-gray-gedd" : ""}`}>{date}</p>
+                    ${isActive ? "!text-gray-gedd" : ""}`}>{date}</p>
             </button>
         </>
     )

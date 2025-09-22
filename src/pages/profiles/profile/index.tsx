143abc63a99dfae4070ee1e71b912b4d04b4d52a
PHET45 <phnjk2000@gmail.com>
2025-09-22 15:00:02 +0700

    reflector: add profileBar to other page

--- src/pages/profiles/profile/index.tsx
+++ src/pages/profiles/profile/index.tsx
@@ -4,10 +4,27 @@ import ProfileBar from '@/components/Widgets/ProfileBar'
 const index = () => {
   return (
     <div className="bg-blue-b flex flex-col min-h-[100dvh]">
-      <NavBarWidget/>
-      <div className='flex flex-row items-center justify-center pt-10'>
-        <div><ProfileBar/></div>
-        <div></div>
+      <NavBarWidget />
+      <div className="flex-1">
+        <div
+          className="
+            flex flex-col md:flex-row 
+            items-start justify-start 
+            pt-10 md:pt-20 
+            px-5 md:px-10 lg:px-100 
+            h-full gap-6 md:gap-10
+          "
+        >
+          {/* ProfileBar */}
+          <div className="w-full md:w-1/4 lg:w-1/5">
+            <ProfileBar />
+          </div>
+
+          {/* Content - Full Width Container */}
+          <div className="flex flex-col items-start justify-start gap-y-5 w-full md:w-3/4 lg:w-4/5 px-0 md:px-0 lg:px-22">
+            
+          </div>
+        </div>
       </div>
     </div>
   )

7d20c4c840172f79aa8f1b828e95a7e3268f85a7
PHET45 <phnjk2000@gmail.com>
2025-09-22 15:10:17 +0700

    feat: My coupons page

--- src/pages/profiles/myCoupon/index.tsx
+++ src/pages/profiles/myCoupon/index.tsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import NavBarWidget from '@/components/Widgets/NavBarWidget'
 import ProfileBar from '@/components/Widgets/ProfileBar'
+import CouponCard from '@/components/Cards/CouponCard'
 const index = () => {
   return (
-   <div className="bg-blue-b flex flex-col min-h-[100dvh]">
+    <div className="bg-blue-b flex flex-col min-h-[100dvh]">
       <NavBarWidget />
       <div className="flex-1">
         <div
@@ -22,7 +23,17 @@ const index = () => {
 
           {/* Content - Full Width Container */}
           <div className="flex flex-col items-start justify-start gap-y-5 w-full md:w-3/4 lg:w-4/5 px-0 md:px-0 lg:px-22">
-            
+            <div className="text-f-24 md:text-f-28 lg:text-f-36 px-0 md:px-0 lg:px-0">
+              My coupons
+            </div>
+            <div className="grid grid-cols-2 md:grid-cols-2  gap-y-10 w-full px-0 md:px-0 lg:px-0">
+            <CouponCard title={'Minor Cineplex x COKE JOYFUL '}/>
+            <CouponCard title={'Minor Cineplex x COKE JOYFUL '}/>
+             <CouponCard title={'Minor Cineplex x COKE JOYFUL '}/>
+             <CouponCard title={'Minor Cineplex x COKE JOYFUL '}/>
+             <CouponCard title={'Minor Cineplex x COKE JOYFUL '}/>
+             <CouponCard title={'Minor Cineplex x COKE JOYFUL '}/>
+            </div>
           </div>
         </div>
       </div>

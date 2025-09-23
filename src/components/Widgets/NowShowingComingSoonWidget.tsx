de6a8201a4d86f753930ba4da3b60711618f6a27
Jue Parinthon <jue.prt@hotmail.com>
2025-09-23 10:27:49 +0700

    fix: change Movies in Coimgsoon movies

--- src/components/Widgets/NowShowingComingSoonWidget.tsx
+++ src/components/Widgets/NowShowingComingSoonWidget.tsx
@@ -1,6 +1,7 @@
 import MovieCard from "../Cards/MovieCard";
 import { useState } from "react";
 
+
 function NowShowingComingSoon() {
   const [activeTab, setActiveTab] = useState("nowShowing");
 
@@ -49,9 +50,9 @@ function NowShowingComingSoon() {
 
   const comingSoonMovies = [
     {
-      title: "Django Unchained",
+      title: "Pirates of the Caribbean: Dead men tell no tales",
       image:
-        "https://m.media-amazon.com/images/M/MV5BMjIyNTQ5NjQ1OV5BMl5BanBnXkFtZTcwODg1MDU4OA@@._V1_FMjpg_UX1000_.jpg",
+        "https://m.media-amazon.com/images/M/MV5BMTYyMTcxNzc5M15BMl5BanBnXkFtZTgwOTg2ODE2MTI@._V1_FMjpg_UX1000_.jpg",
       date: "24 Jun 2024",
       rating: 4.6,
       genreTag1: "Comedy",
@@ -59,9 +60,9 @@ function NowShowingComingSoon() {
       langTag: "EN",
     },
     {
-      title: "The Dark Knight",
+      title: "Sherlock Holmes: A Game of Shadows",
       image:
-        "https://m.media-amazon.com/images/M/MV5BMTMxNTMwODM0NF5BMl5BanBnXkFtZTcwODAyMTk2Mw@@._V1_.jpg",
+        "https://m.media-amazon.com/images/M/MV5BMTQwMzQ5Njk1MF5BMl5BanBnXkFtZTcwNjIxNzIxNw@@._V1_.jpg",
       date: "18 Jun 2024",
       rating: 4.6,
       genreTag1: "Action",
@@ -69,9 +70,9 @@ function NowShowingComingSoon() {
       langTag: "TH",
     },
     {
-      title: "Interstellar",
+      title: "Ocean's Eleven",
       image:
-        "https://m.media-amazon.com/images/M/MV5BYzdjMDAxZGItMjI2My00ODA1LTlkNzItOWFjMDU5ZDJlYWY3XkEyXkFqcGc@._V1_FMjpg_UX1000_.jpg",
+        "https://m.media-amazon.com/images/M/MV5BMmNhZDkxYTgtMDM3ZC00NTQ3LWFjZTUtNzc1Y2QyNWZjNDRmXkEyXkFqcGc@._V1_FMjpg_UX1000_.jpg",
       date: "10 Jul 2024",
       rating: 4.8,
       genreTag1: "Sci-fi",
@@ -79,9 +80,9 @@ function NowShowingComingSoon() {
       langTag: "TH/EN",
     },
     {
-      title: "Dune: Part Two",
+      title: "300",
       image:
-        "https://m.media-amazon.com/images/M/MV5BNTc0YmQxMjEtODI5MC00NjFiLTlkMWUtOGQ5NjFmYWUyZGJhXkEyXkFqcGc@._V1_FMjpg_UX1000_.jpg",
+        "https://m.media-amazon.com/images/M/MV5BMjc4OTc0ODgwNV5BMl5BanBnXkFtZTcwNjM1ODE0MQ@@._V1_FMjpg_UX1000_.jpg",
       date: "20 Jul 2024",
       rating: 4.9,
       genreTag1: "Action",
@@ -105,10 +106,18 @@ function NowShowingComingSoon() {
         </div>
 
         <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-5">
-          <MovieCard title="Django Unchained" image="https://m.media-amazon.com/images/M/MV5BMjIyNTQ5NjQ1OV5BMl5BanBnXkFtZTcwODg1MDU4OA@@._V1_FMjpg_UX1000_.jpg" date="24 Jun 2024" rating={4.6} genreTag1="Comedy" genreTag2="Drama" langTag="EN" />
-          <MovieCard title="The Dark Knight" image="https://m.media-amazon.com/images/M/MV5BMTMxNTMwODM0NF5BMl5BanBnXkFtZTcwODAyMTk2Mw@@._V1_.jpg" date="18 Jun 2024" rating={4.6} genreTag1="Action" genreTag2="Crime" langTag="TH" />
-          <MovieCard title="Interstellar" image="https://m.media-amazon.com/images/M/MV5BYzdjMDAxZGItMjI2My00ODA1LTlkNzItOWFjMDU5ZDJlYWY3XkEyXkFqcGc@._V1_FMjpg_UX1000_.jpg" date="24 Jun 2024" rating={4.6} genreTag1="Sci-fi" genreTag2="Drama" langTag="TH/EN" />
-          <MovieCard title="Dune: Part Two" image="https://m.media-amazon.com/images/M/MV5BNTc0YmQxMjEtODI5MC00NjFiLTlkMWUtOGQ5NjFmYWUyZGJhXkEyXkFqcGc@._V1_FMjpg_UX1000_.jpg" date="24 Jun 2024" rating={4.6} genreTag1="Action" genreTag2="Drama" langTag="TH/EN" />
+          {moviesToDisplay.map((movie, index) => (
+            <MovieCard
+            key={index}
+            title={movie.title}
+            image={movie.image}
+            date={movie.date}
+            rating={movie.rating}
+            genreTag1={movie.genreTag1}
+              genreTag2={movie.genreTag2}
+              langTag={movie.langTag}
+           />
+          ))}
         </div>
       </div>
     </div>

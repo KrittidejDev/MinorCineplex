c03d43484cdbd7d599093ae8450b420857fbdef2
Supakit Jaratworapat <supakit.jaratworapat@gmail.com>
2025-09-29 10:20:16 +0700

    fix: improve error handling in signup API by defining ServiceError type and ensuring consistent response structure

--- src/pages/api/auth/signup.ts
+++ src/pages/api/auth/signup.ts
@@ -1,6 +1,11 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 import { registerUser } from "../../../services/authService";
 
+type ServiceError = {
+  status: number;
+  message: string;
+};
+
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
@@ -8,14 +13,14 @@ export default async function handler(
   try {
     const user = await registerUser(req.body);
     res.status(201).json({ message: "Register successfully", user });
-  } catch (err: any) {
-    console.error("Register failed:", err);
-
-    if (err.status) {
+  } catch (err: unknown) {
+    const error = err as ServiceError;
+    if (error.status) {
       return res
-        .status(err.status)
-        .json({ status: err.status, message: err.message });
+        .status(error.status)
+        .json({ status: error.status, message: error.message });
     }
+    return res.status(500).json({ error: "Server Error" });
   }
   return res.status(500).json({ error: "Server Error" });
 }

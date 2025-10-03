3098de76a0faa03118c7c850e1d6aa0f5504b7ee
plawan1995 <jnyjame12@gmail.com>
2025-10-03 16:36:58 +0700

    feat: implement CreateNewCinemaCard component with modal functionality in AdminCinemaWidgets

--- src/components/Widgets/AdminCinemaWidgets.tsx
+++ src/components/Widgets/AdminCinemaWidgets.tsx
@@ -1,22 +1,33 @@
 import AddRoundLight from "../Icons/AddRoundLight"
 import { Button } from "../ui/button"
 import TableCard from "../Cards/TableCard"
-
-
-
+import CreateNewCinemaCard from "../Cards/CreateNewCinemaCard"
+import { useState } from "react"
 
 export default function AdminCinemaWidgets() {
+    const [isShowCreateModal, setIsShowCreateModal] = useState(false)
+
     return (
         <div className="flex flex-col gap-10">
             <div className="flex items-center justify-between mt-20 mx-[70px]">
                 <h1 className="text-gray-g63f text-f-56 font-bold">Cinema Branch</h1>
-                <Button className="btn-base blue-normal text-fm-16 font-bold px-4 py-3 gap-2.5"><AddRoundLight width={24} height={40} color={"#FFFFFF"} />Add Cinema</Button>
+                <Button
+                    className="btn-base blue-normal text-fm-16 font-bold px-4 py-3 gap-2.5"
+                    onClick={() => setIsShowCreateModal(true)}
+                >
+                    <AddRoundLight width={24} height={40} color={"#FFFFFF"} />
+                    Add Cinema
+                </Button>
             </div>
             <div className="mx-[70px]">
                 <TableCard />
             </div>
-            
+
+            {/* Create New Cinema Modal */}
+            <CreateNewCinemaCard
+                isShowModal={isShowCreateModal}
+                onClose={() => setIsShowCreateModal(false)}
+            />
         </div>
     )
-
 }

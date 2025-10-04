ef99dc1f158f4e47f95b22cb26e3b703f23992fa
Jue Parinthon <jue.prt@hotmail.com>
2025-10-05 03:10:53 +0700

    feat: add DateSelectionWidget into MovieInfoWidget

--- src/components/Widgets/MovieInfoWidget.tsx
+++ src/components/Widgets/MovieInfoWidget.tsx
@@ -7,6 +7,7 @@ import TrailerPlayer from "../Displays/TrailerPlayer";
 import { ActorProfile, DirectorProfile } from "../ui/actordirectorlist";
 import InputSearch from "../Inputs/InputSearch";
 import CitySelection from "../ui/cityselection";
+import DateSelectionBarWidget from "./DateSelectionBarWidget";
 
 interface MoviesDetailWidgetProps {
   movie: APIMovie;
@@ -14,40 +15,43 @@ interface MoviesDetailWidgetProps {
 
 const MovieInfoWidget: React.FC<MoviesDetailWidgetProps> = ({ movie }) => {
   const [activeTab, setActiveTab] = useState("ข้อมูลภาพยนต์");
+  const [selectedDate, setSelectedDate] = useState<Date>(new Date());
   if (!movie) return <p>ไม่พบข้อมูลหนัง</p>;
 
   return (
     <div>
-      <div className="flex justify-between">
-        <MovieCard
-          id={movie.id}
-          title={movie.title}
-          poster_url={movie.poster_url || undefined}
-          release_date={
-            movie.release_date ? new Date(movie.release_date) : undefined
-          }
-          rating={movie.rating || undefined}
-          genre={movie.genre || undefined}
-        />
-        <TrailerPlayer url={movie.trailer_url} />
-      </div>
-      <div className="flex gap-10 mt-10">
-        <button
-          className={`text-[20px] font-semibold cursor-pointer ${activeTab === "ข้อมูลภาพยนต์" ? "text-blue-bbee" : "text-white-wfff"}`}
-          onClick={() => setActiveTab("ข้อมูลภาพยนต์")}
-        >
-          ข้อมูลภาพยนต์
-        </button>
-        <button
-          className={`text-[20px] font-semibold cursor-pointer ${activeTab === "รอบฉาย" ? "text-blue-bbee" : "text-white-wfff"}`}
-          onClick={() => setActiveTab("รอบฉาย")}
-        >
-          รอบฉาย
-        </button>
+      <div className="max-w-[1200px] mx-auto">
+        <div className="flex justify-between">
+          <MovieCard
+            id={movie.id}
+            title={movie.title}
+            poster_url={movie.poster_url || undefined}
+            release_date={
+              movie.release_date ? new Date(movie.release_date) : undefined
+            }
+            rating={movie.rating || undefined}
+            genre={movie.genre || undefined}
+          />
+          <TrailerPlayer url={movie.trailer_url} />
+        </div>
+        <div className="flex gap-10 mt-10">
+          <button
+            className={`text-[20px] font-semibold cursor-pointer ${activeTab === "ข้อมูลภาพยนต์" ? "text-blue-bbee" : "text-white-wfff"}`}
+            onClick={() => setActiveTab("ข้อมูลภาพยนต์")}
+          >
+            ข้อมูลภาพยนต์
+          </button>
+          <button
+            className={`text-[20px] font-semibold cursor-pointer ${activeTab === "รอบฉาย" ? "text-blue-bbee" : "text-white-wfff"}`}
+            onClick={() => setActiveTab("รอบฉาย")}
+          >
+            รอบฉาย
+          </button>
+        </div>
       </div>
 
       {activeTab === "ข้อมูลภาพยนต์" && (
-        <div className="flex flex-col gap-10 mt-10">
+        <div className="max-w-[1200px] mx-auto flex flex-col gap-10 mt-10">
           <div>
             <h3 className="font-bold text-f-24">นักแสดง</h3>
             <div className="flex gap-2.5 mt-5">
@@ -82,6 +86,12 @@ const MovieInfoWidget: React.FC<MoviesDetailWidgetProps> = ({ movie }) => {
             <div className="w-full lg:w-[285px]">
               <CitySelection />
             </div>
+          </div>
+
+          <div className="w-full mt-10">
+            <DateSelectionBarWidget
+              onSelectDate={(date) => setSelectedDate(date)}
+            />
           </div>
         </div>
       )}

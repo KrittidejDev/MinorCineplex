afb7f6b29eb5879ccbf1adcc212b87bc354cb84f
Supakit Jaratworapat <supakit.jaratworapat@gmail.com>
2025-10-10 00:42:35 +0700

    feat: enhance AdminShowtime components with dynamic filtering and API integration
    - Updated AdminShowtimeFilter to utilize AdminInputDropdown for improved input handling.
    - Refactored AdminShowtimeWidget to accept data as props and display showtime information dynamically.
    - Implemented API call in AdminShowtime page to fetch showtime data based on user-defined filters.
    - Enhanced showtime data structure to include movie, cinema, hall, and timeslot details.

--- src/components/Widgets/AdminShowtimeWidget.tsx
+++ src/components/Widgets/AdminShowtimeWidget.tsx
@@ -4,42 +4,26 @@ import TableCard from "@/components/Cards/TableCard";
 import AdminShowtimeFilter from "./AdminShowtimeFilter";
 import DateSelectionBarWidget from "./DateSelectionBarWidget";
 
-const AdminShowtimeWidget = () => {
+interface AdminShowtimeWidgetProps {
+  data: ShowTimeData[];
+}
+
+export interface ShowTimeData {
+  id: string;
+  movie: string;
+  cinema: string;
+  hall: string;
+  timeslot: string;
+}
+
+const AdminShowtimeWidget = ({ data }: AdminShowtimeWidgetProps) => {
   const showtimeColumns = [
     { key: "movie", label: "Movie Name", align: "left" as const },
     { key: "cinema", label: "Cinema", align: "left" as const },
     { key: "hall", label: "Hall", align: "left" as const },
     { key: "timeslot", label: "Timeslot", align: "left" as const },
   ];
-  const showtimeData = [
-    {
-      id: "1",
-      movie: "Movie 1",
-      cinema: "Cinema 1",
-      hall: "Hall 1",
-      timeslot: "Timeslot 1",
-      created_at: "2021-01-01",
-      updated_at: "2021-01-01",
-    },
-    {
-      id: "2",
-      movie: "Movie 3",
-      cinema: "Cinema 3",
-      hall: "Hall 3",
-      timeslot: "Timeslot 1",
-      created_at: "2021-01-01",
-      updated_at: "2021-01-01",
-    },
-    {
-      id: "3",
-      movie: "Movie 2",
-      cinema: "Cinema 2",
-      hall: "Hall 2",
-      timeslot: "Timeslot 1",
-      created_at: "2021-01-01",
-      updated_at: "2021-01-01",
-    },
-  ];
+
   const showtimeActions = [
     {
       onView: () => console.log("View Movie 1"),
@@ -53,24 +37,30 @@ const AdminShowtimeWidget = () => {
     },
   ];
 
+  console.log(data);
+
   return (
     <div className="flex flex-col gap-10">
-      <div className="flex justify-between items-center">
+      <div className="flex justify-between items-center px-15 mt-20">
         <h1 className="text-f-56 text-gray-g63f">Showtime</h1>
         <Button className="btn-base blue-normal text-fm-16 font-bold px-4 py-3 gap-2.5">
           <AddRoundLight width={24} height={24} color={"#FFFFFF"} />
           Add Showtime
         </Button>
       </div>
-      <AdminShowtimeFilter />
-      <DateSelectionBarWidget onSelectDate={() => {}} />
-      <TableCard
-        columns={showtimeColumns}
-        data={showtimeData}
-        actions={showtimeActions}
-      />
+      <div className="px-15">
+        {/* <AdminShowtimeFilter data={data} /> */}
+      </div>
+      {/* <DateSelectionBarWidget onSelectDate={() => {}} /> */}
+      <div className="w-full flex flex-col gap-10">
+        <TableCard
+          columns={showtimeColumns}
+          data={data}
+          actions={showtimeActions}
+        />
+      </div>
       <div className="flex justify-start">
-        <span className="text-fm-14 text-gray-g63f">{`Showing ${showtimeData.length} of ${showtimeData.length} results`}</span>
+        <span className="text-fm-14 text-gray-g63f">{`Showing ${data.length} of ${data.length} results`}</span>
       </div>
     </div>
   );
--- src/pages/admin/showtime.tsx
+++ src/pages/admin/showtime.tsx
@@ -1,15 +1,44 @@
 import AdminSidebar from "@/components/ui/adminsidebar";
 import AdminShowtimeWidget from "@/components/Widgets/AdminShowtimeWidget";
+import axios from "axios";
+import { useState,useEffect } from "react";
+
 
 export default function AdminShowtime() {
+  const [page, setPage] = useState(1);
+  const [limit, setLimit] = useState(10);
+  const [query, setQuery] = useState({
+    movie: "Your Letter",
+    cinema: "เมเจอร์ ทีเอ็มเค กาญจนบุรี",
+    hall: "",
+  });
+  const [data, setData] = useState([]);
+  const getShowtime = async () => {
+    const {data} = await axios.get(`http://localhost:3000/api/showtimes`,{
+      params: {
+        limit: limit,
+        page: page,
+        movie: query.movie,
+        cinema: query.cinema,
+        hall: query.hall
+      },
+    });
+    setData(data.showTimes);
+  };
+  useEffect(() => {
+    getShowtime();
+  }, [page, limit, query]);
+console.log(data);
+
+
   return (
     <div className="bg-white-wfff">
       <div className="flex">
         <div>
         <AdminSidebar />
       </div>
-      <div className="w-full px-15 py-20">
-        <AdminShowtimeWidget />
+      <div className="w-full">
+        <AdminShowtimeWidget data={data} />
       </div>
       </div>
     </div>

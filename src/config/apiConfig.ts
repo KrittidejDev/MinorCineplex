cb2d9dbcdb67200e43918d4a4e6c6038863e5702
KrittidejDev <krittidej.dev@gmail.com>
2025-10-16 09:26:50 +0700

    fix api error

--- src/config/apiConfig.ts
+++ src/config/apiConfig.ts
@@ -1,7 +1,7 @@
 // Development
 // export const BASE_API = "http://localhost:4000";
 // export const BASE_API = "http://localhost:3000";
-export const BASE_API = "minor-cineplex-phi.vercel.app";
+// export const BASE_API = "minor-cineplex-phi.vercel.app";
 
 // Production
 // export const BASE_API = "https://personal-backend-seven.vercel.app";

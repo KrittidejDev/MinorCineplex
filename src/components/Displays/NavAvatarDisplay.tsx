949b9ed75e54b4deb7e3e974a785b5c4fc2a21d0
PHET45 <phnjk2000@gmail.com>
2025-10-17 12:13:38 +0700

    refactor: add cursor-pointer class to various components for enhanced user interaction
    - Updated NavAvatarDisplay, CinemaDetallWidget, NavbarMenu, and NavBarWidget components to include the cursor-pointer class, improving visual feedback for users on hover.
    - Minor adjustments made to class names for consistency and clarity.

--- src/components/Displays/NavAvatarDisplay.tsx
+++ src/components/Displays/NavAvatarDisplay.tsx
@@ -20,12 +20,12 @@ export const AvatarDisplay: React.FC<AvatarDisplayProps> = ({
   return (
     <div className={`${className} relative`} >
       <div className="flex items-center gap-x-2.5 " onClick={_handleOpen}>
-        <div className={"w-12 h-12 rounded-full overflow-hidden"}>
+        <div className={"w-12 h-12 rounded-full overflow-hidden cursor-pointer"}>
           {data?.avatar_url ? (
             <Image
               src={data.avatar_url}
               alt="avatar"
-              className="object-cover object-center w-full h-full"
+              className="object-cover object-center w-full h-full cursor-pointer"
               width={48}
               height={48}
             />
@@ -33,8 +33,8 @@ export const AvatarDisplay: React.FC<AvatarDisplayProps> = ({
             <UserDuotone width={48} height={48} />
           )}
         </div>
-        <div className="flex flex-nowrap items-center gap-x-1.5 text-white">
-          <span className=" max-w-[10ch] overflow-hidden whitespace-nowrap text-ellipsis">
+        <div className="flex flex-nowrap items-center gap-x-1.5 text-white cursor-pointer">
+          <span className=" max-w-[10ch] overflow-hidden whitespace-nowrap text-ellipsis cursor-pointer">
             {data?.username}
           </span>
           <ArrowDown />

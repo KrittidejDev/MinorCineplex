2f556d1709032804dff7b82368413ffd756cd208
Supakit Jaratworapat <supakit.jaratworapat@gmail.com>
2025-10-24 16:52:29 +0700

    feat: add social sharing functionality with ShareButton and associated icons
    - Introduced ShareButton component to facilitate sharing via Facebook, Messenger, Twitter, and Line.
    - Added FacebookIcon, LineIcon, MessengerIcon, and TwitterIcon components for social media sharing.
    - Updated OutLight and CopyLight icons to support click events.
    - Integrated ShareModal for displaying sharing options when the ShareButton is clicked.
    - Enhanced CopyLight to include an onClick prop for better interactivity.

---
+++ src/components/Widgets/ShareButton.tsx
@@ -0,0 +1,68 @@
+import OutLight from "../Icons/OutLight";
+import { useState } from "react";
+import ShareModal from "./ShareModal";
+import { toast } from "react-toastify";
+const ShareButton = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleShareFacebook = () => {
+    const url = window.location.href;
+    const facebookUrl = `https://www.facebook.com/sharer/sharer.php?u=${encodeURIComponent(url)}`;
+    window.open(facebookUrl, "_blank", "width=600,height=400");
+  };
+  const handleShareMessenger = () => {
+    navigator.clipboard.writeText(window.location.href);
+    window.open("https://www.messenger.com", "_blank", "width=600,height=400");
+  };
+  const handleShareTwitter = () => {
+    const url = window.location.href;
+    const text = "Let's watch together! Join me & book now! ";
+    const twitterUrl = `https://twitter.com/intent/tweet?text=${encodeURIComponent(text + " " + url)}`;
+    window.open(twitterUrl, "_blank", "width=600,height=400");
+  };
+  const handleShareLine = () => {
+    const url = window.location.href;
+    const text = "Let's watch together! Join me & book now! ";
+    const lineUrl = `https://line.me/R/msg/text/?${encodeURIComponent(text + "\n\n" + url)}`;
+    window.open(lineUrl, "_blank", "width=600,height=400");
+  };
+
+  const handleShareCopyLink = () => {
+    navigator.clipboard.writeText(window.location.href);
+    toast.success("Link copied to clipboard");
+  };
+
+  const handleShare = () => {
+    setIsOpen(!isOpen);
+  };
+  return (
+    <div
+      className="flex items-center justify-center gap-1.5"
+      onClick={handleShare}
+    >
+      <OutLight
+        width="30"
+        height="30"
+        color="white"
+        className="cursor-pointer"
+      />
+      <span className="text-fm-16 font-bold text-white hover:underline cursor-pointer">
+        Share This Booking
+      </span>
+      {isOpen && (
+        <div className="w-ful max-w-[432px]">
+          <ShareModal
+            isOpen={isOpen}
+            handleShare={handleShare}
+            handleShareFacebook={handleShareFacebook}
+            handleShareMessenger={handleShareMessenger}
+            handleShareTwitter={handleShareTwitter}
+            handleShareCopyLink={handleShareCopyLink}
+            handleShareLine={handleShareLine}
+          />
+        </div>
+      )}
+    </div>
+  );
+};
+export default ShareButton;
